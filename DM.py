ea95788726381b6963e7b99fc7cddb8976877ca4
neinsys <sys7961@gmail.com>
2017-01-07 23:30:18 +0900

    add shortcut

---
+++ DM.py
@@ -0,0 +1,11 @@
+#it is shortcut using datamaker
+
+from datamaker import *
+
+DT=DataType
+Seq=Sequence
+G=Graph
+BG=BipartiteGraph
+DM=DataMaker
+LDM=LinearDataMaker
+GDM=GroupDataMaker
---
+++ main.py
@@ -0,0 +1,35 @@
+
+
+
+def main():
+    from DM import *
+
+    group = [
+        {
+            'low': 1,
+            'high': 10,
+            'num': 5,
+            'lowercase':True
+        },
+        {
+            'low': 10,
+            'high': 100,
+            'num': 5,
+            'lowercase': True
+        }
+    ]
+    GDM(Grid, group)
+
+
+
+if __name__=="__main__":
+    main()
+
+
+
+
+
+
+
+
+
